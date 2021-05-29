4cb79719e2d13393308136aaa037e3f9eb4d4184
Gautam Kumar Pandey <gautamk780@gmail.com>
2021-05-29 21:54:12 +0530

    Update Github extract v4.py

--- Github extract v4.py
+++ Github extract v4.py
@@ -1,8 +1,9 @@
+__author__ = 'Gautam'
 
 # this will extract complete user info on command prompt on URl Based
 
 #https://github.com/search?q=%22linkedin%22+OR+Hadoop&type=Users
-# Keyword --> "linkedin" OR Hadoop
+# Extraction            : Extraction from Required URL
 # Script Name           : Github extract.py
 # Author                : Gautam Kumar Pandey
 # Created               : 17th May 2019
@@ -24,14 +25,15 @@ def main():
     sign_in = driver.find_element_by_xpath('/html/body/div[1]/header/div/div[2]/div[2]/a[1]')
     sign_in.click()
     userid = driver.find_element_by_id("login_field")
-    userid.send_keys('*******')
+    userid.send_keys('gautamk780@gmail.com')
     password = driver.find_element_by_xpath('//*[@id="password"]')
-    password.send_keys('*******')
-    submit = driver.find_element_by_xpath('//*[@id="login"]/form/div[3]/input[8]')
+    password.send_keys('gitpygautam123')
+    submit = driver.find_element_by_xpath('//*[@id="login"]/div[4]/form/div/input[12]')  
     submit.click() 
     time.sleep(5)
     
-    driver.get('https://github.com/search?l=Java&p=25&q=%22BIg+Data%22&type=Users')
+    # put new required Url
+    driver.get('https://github.com/search?q=Data+Scientist+location%3A%22South+Africa%22&type=Users&ref=advsearch&l=&l=')
 
     #___________________Done_______________________#
 
@@ -40,9 +42,9 @@ def main():
     while pages_remaining:
         soup = bs.BeautifulSoup(driver.page_source,'lxml')
         #Data from users
-        for users in soup.find_all(class_="user-list-item py-4 d-flex hx_hit-user"):  #previous  user-list-info ml-2 min-width-0
+        for users in soup.find_all(class_="d-flex hx_hit-user px-0 Box-row"):  #previous  user-list-info ml-2 min-width-0
             print(users.text)
-            print("+-"*30,"\n\n")
+            print("*"*30,"\n\n")
             
         try:
             #Checks if there are more pages with links
